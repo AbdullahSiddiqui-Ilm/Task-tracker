dfcbc1f8ac38442f63ec9582969fce2d5c3e295a
Abdullah Siddiqui <abzsidd4@gmail.com>
2026-07-02 10:53:36 +0100

    Change functions to update json file

--- task_tracker/task_tracking.py
+++ task_tracker/task_tracking.py
@@ -2,23 +2,11 @@
 import datetime
 import argparse
 import json
-from task_tracking import task_db
 
 task_db = []
+with open("tasks.json", "w") as f:
+    json.dump(task_db, f)
 
-jsonfile = open('tasks.json', 'w')
-json.dump(task_db, jsonfile)
-jsonfile.close()
-
-if not jsonfile:
-    jsonfile = open('tasks.json', 'w')
-    json.dump(task_db, jsonfile)
-    jsonfile.close()
-else:
-    pass
-
-def __init__(self):
-    pass
 
 def add_task(id, task, status):
     task_db.append({
@@ -26,29 +14,35 @@ def add_task(id, task, status):
         "task": task,
         "status": status
     })
+    with open("tasks.json", "w") as file:
+        json.dump(task_db, file, indent=4)
+
 
 def update_task(id, new_task):
     for i in task_db:
         if i['id'] == id:
             i['task'] = new_task
-    return task_db
+    with open("tasks.json", "w") as file:
+        json.dump(task_db, file, indent=4)
+
 
 def update_status(id, new_status):
     for i in task_db:
         if i['id'] == id:
             i['status'] = new_status
-    return task_db
+    with open("tasks.json", "w") as file:
+        json.dump(task_db, file, indent=4)
+
 
 def delete_task(id):
     for i in task_db:
         if i['id'] == id:
             task_db.remove(i)
             break
-    return task_db
+    with open("tasks.json", "w") as file:
+        json.dump(task_db, file, indent=4)
 
 add_task(1, 'eat chicken', 'in progress')
 add_task(2, 'eat lamb', 'in progress')
-
-print(delete_task(2))
             
 
