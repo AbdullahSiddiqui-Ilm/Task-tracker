4c172d6c1919582e468fd1dc0857144844a29b0e
Abdullah Siddiqui <abzsidd4@gmail.com>
2026-07-08 12:45:48 +0100

    Finish Update task CLI implementation
    I finished the implementation of updating a task
    through the CLI. There are also many other changes
    that took place. random_id was returning a string,
    which was causing a TypeError, as the json file
    was storing its IDs as strings. This created an issue
    as every time i entered an ID as an integer on the
    CLI to be updated, it would return the "Id was not found"
    string. This lead to the TypeError:
    "TypeError: string indices must be integers, not 'str'",
    as the elif statement for args.command would always
    return a string, whereas i expected:
    "print(f"Task auccessfully updated, ID {updated_task['id']}")"
    to return a dictionary.
    
    The fix revolved around one main change, which was
    to make sure that random_id returns an int.

--- task_tracker/task_tracking.py
+++ task_tracker/task_tracking.py
@@ -16,7 +16,7 @@ if not os.path.exists("tasks.json"):
 def add_task(task_description, status="todo"):
     with open("tasks.json", "r") as f:
         task_db_temp = json.load(f)  
-    random_id = ''.join(random.choices(string.digits, k=6))
+    random_id = int(''.join(random.choices(string.digits, k=6)))
     task = {
         "id": random_id,
         "task": task_description,
@@ -37,9 +37,9 @@ def update_task(id, new_task):
         for i in task_db_temp:
             if i['id'] == id:
                 i['task'] = new_task
-                return i
     with open("tasks.json", "w") as file:
         json.dump(task_db_temp, file, indent=4)
+    return i
 
 
 
@@ -81,21 +81,29 @@ parser_update_status = subparsers.add_parser("update-status")
 parser_delete = subparsers.add_parser("delete")
 
 parser_add.add_argument("task", nargs="?", default=None)
-parser_update_task.add_argument("updateTaskID", nargs="?", default=None)
-parser_update_task.add_argument("--updateTaskName")
-parser_update_status.add_argument("--updateStatusID")
+
+parser_update_task.add_argument("updateTaskID", type=int)
+parser_update_task.add_argument("updateTaskName")
+
+parser_update_status.add_argument("--updateStatusID", type=int)
+
 parser_delete.add_argument("--deleteTask")
-
-
 args = parser.parse_args()
 
-if args.task:
+if args.command == "add":
     added_task = add_task(args.task)
     print(f"Task successfully added, ID: {added_task['id']}")
-
-if args.updateTaskID:
+elif args.command == "update-task":
     updated_task = update_task(args.updateTaskID, args.updateTaskName)
-    print(f"Task successfully updated, ID: {updated_task['id']}")
+    if updated_task == str:
+        print(updated_task)
+    else:
+        print(f"Task auccessfully updated, ID {updated_task['id']}")
+   
+""" if updated_task == str:
+        print(updated_task)
+    else:
+        print(f"Task successfully updated, ID: {updated_task['id']}") """
 
 
 
