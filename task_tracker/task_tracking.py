1101472fd05569df21b8a6dcd8f44547f512a9ec
Abdullah Siddiqui <abzsidd4@gmail.com>
2026-07-08 03:52:59 +0100

    Try update logic for CLI

--- task_tracker/task_tracking.py
+++ task_tracker/task_tracking.py
@@ -29,57 +29,75 @@ def add_task(task_description, status="todo"):
 
 
 def update_task(id, new_task):
-    if not any(i["id"] == id for i in task_db):
-        print("ID was not found")
+    with open ("tasks.json", "r") as f:
+        task_db_temp = json.load(f)
+    if not any(i["id"] == id for i in task_db_temp):
+        return "ID was not found"
     else:
-        for i in task_db:
+        for i in task_db_temp:
             if i['id'] == id:
                 i['task'] = new_task
-        else:
-            print("ID does not exist in task records")
+                return i
     with open("tasks.json", "w") as file:
-        json.dump(task_db, file, indent=4)
+        json.dump(task_db_temp, file, indent=4)
+
 
 
 def update_status(id, new_status):
-    if not any(i["id"] == id for i in task_db):
+    with open ("tasks.json", "r") as f:
+        task_db_temp = json.load(f)
+    if not any(i["id"] == id for i in task_db_temp):
         print("ID was not found")
     else:
-        for i in task_db:
+        for i in task_db_temp:
             if i['id'] == id:
                 i['status'] = new_status
+                task_db_temp.append(i)
     with open("tasks.json", "w") as file:
-        json.dump(task_db, file, indent=4)
+        json.dump(task_db_temp, file, indent=4)
 
 
 def delete_task(id):
-    if not any(i["id"] == id for i in task_db):
+    with open ("tasks.json", "r") as f:
+        task_db_temp = json.load(f)
+    if not any(i["id"] == id for i in task_db_temp):
         print("ID was not found")
     else:
-        for task in task_db:
+        for task in task_db_temp:
             if task['id'] == id:
-                task_db.remove(task)
+                task_db_temp.remove(task)
             break
     with open("tasks.json", "w") as file:
         json.dump(task_db, file, indent=4)
 
 
         
-
-
-
 parser = argparse.ArgumentParser()
-
 subparsers = parser.add_subparsers(dest="command")
 
 parser_add = subparsers.add_parser("add")
-parser_add.add_argument("taskDescription")
+parser_update_task = subparsers.add_parser("update-task")
+parser_update_status = subparsers.add_parser("update-status")
+parser_delete = subparsers.add_parser("delete")
+
+parser_add.add_argument("task", nargs="?", default=None)
+parser_update_task.add_argument("updateTaskID", nargs="?", default=None)
+parser_update_task.add_argument("--updateTaskName")
+parser_update_status.add_argument("--updateStatusID")
+parser_delete.add_argument("--deleteTask")
+
 
 args = parser.parse_args()
 
-if args.taskDescription:
-    result = add_task(args.taskDescription)
-    print(f"Task successfully added, ID: {result['id']}")
+if args.task:
+    added_task = add_task(args.task)
+    print(f"Task successfully added, ID: {added_task['id']}")
+
+if args.updateTaskID:
+    updated_task = update_task(args.updateTaskID, args.updateTaskName)
+    print(f"Task successfully updated, ID: {updated_task['id']}")
+
+
 
        
 
