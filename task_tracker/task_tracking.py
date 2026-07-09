dd02eba2ddcbf5bd4ee3b57b907c9ea75705963a
Abdullah Siddiqui <abzsidd4@gmail.com>
2026-07-09 12:44:27 +0100

    Finish Update status and delete task

--- task_tracker/task_tracking.py
+++ task_tracker/task_tracking.py
@@ -44,31 +44,32 @@ def update_task(id, new_task):
 
 
 def update_status(id, new_status):
+    new_status.lower()
     with open ("tasks.json", "r") as f:
         task_db_temp = json.load(f)
     if not any(i["id"] == id for i in task_db_temp):
-        print("ID was not found")
+        return "ID was not found"
     else:
         for i in task_db_temp:
             if i['id'] == id:
                 i['status'] = new_status
-                task_db_temp.append(i)
     with open("tasks.json", "w") as file:
         json.dump(task_db_temp, file, indent=4)
+    return i
 
 
 def delete_task(id):
     with open ("tasks.json", "r") as f:
         task_db_temp = json.load(f)
     if not any(i["id"] == id for i in task_db_temp):
-        print("ID was not found")
+        return "ID was not found"
     else:
         for task in task_db_temp:
             if task['id'] == id:
                 task_db_temp.remove(task)
-            break
     with open("tasks.json", "w") as file:
-        json.dump(task_db, file, indent=4)
+        json.dump(task_db_temp, file, indent=4)
+    return task
 
 
         
@@ -78,17 +79,22 @@ subparsers = parser.add_subparsers(dest="command")
 parser_add = subparsers.add_parser("add")
 parser_update_task = subparsers.add_parser("update-task")
 parser_update_status = subparsers.add_parser("update-status")
-parser_delete = subparsers.add_parser("delete")
+parser_delete = subparsers.add_parser("delete-task")
 
 parser_add.add_argument("task", nargs="?", default=None)
 
 parser_update_task.add_argument("updateTaskID", type=int)
 parser_update_task.add_argument("updateTaskName")
 
-parser_update_status.add_argument("--updateStatusID", type=int)
+parser_update_status.add_argument("updateStatusID", type=int)
+parser_update_status.add_argument("updateStatus")
+
+parser_delete.add_argument("deleteTaskID", type=int)
+
 
 parser_delete.add_argument("--deleteTask")
 args = parser.parse_args()
+
 
 if args.command == "add":
     added_task = add_task(args.task)
@@ -99,6 +105,22 @@ elif args.command == "update-task":
         print(updated_task)
     else:
         print(f"Task auccessfully updated, ID {updated_task['id']}")
+elif args.command == "update-status":
+    updated_status = update_status(args.updateStatusID, args.updateStatus)
+    if updated_status == "ID was not found":
+        print(updated_status)
+    elif updated_status['status'] == "in progress":
+        print(f"Updated status for task {updated_status['id']} to {updated_status['status']}")
+    elif updated_status['status'] == "done":
+        print(f"Updated status for task {updated_status['id']} to: {updated_status['status']}")
+elif args.command == "delete-task":
+    deleted_task = delete_task(args.deleteTaskID)
+    if deleted_task == str:
+        print(deleted_task)
+    elif deleted_task == dict:
+        print(f"Task {deleted_task['id']} has been deleted")
+    else:
+        print("There are no tasks to be deleted.")
 
 
 
